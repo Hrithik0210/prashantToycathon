9f44856fb9022d1d6182f7d74d1e4574442a8bea
Hrithik0210 <79349648+Hrithik0210@users.noreply.github.com>
2021-06-22 18:00:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// // alert("hello");
+// var image1 = document.querySelector(".main-item img");
+// const imgurl= "chit.png";
+// $(".btn1").on("click", function () {
+//     image1.setAttribute("src", imgurl);
+
+// });
